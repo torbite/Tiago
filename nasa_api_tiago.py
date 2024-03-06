7ad57d6efec3536f5508c34478f390408a0dbfd3
torbite <89304412+torbite@users.noreply.github.com>
2024-03-06 11:54:02 -0300

    Add files via upload

--- nasa_api_tiago.py
+++ nasa_api_tiago.py
@@ -16,15 +16,16 @@ RA: 23011105
 MAHLI
 """
 
-import requests, json
+import requests
 
 
 
 r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2000&camera=MAHLI&api_key=n7eHVk1JAmkVdbJxgA8py91hFxNWCA6vstKSFE9B')
 
-print(dict(r.json())['photos'][1]['img_src'])
+
 
 lst = []
 for i in range(len(dict(r.json())['photos'])):
   lst.append(dict(r.json())['photos'][i]['img_src'])
-print(f'the links are: {lst}')
+print(f'Os links são: {lst}')
+print(f'Link da primeira imagem: {dict(r.json())["photos"][1]["img_src"]}')
